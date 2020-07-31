e46fee31c78c50a2e8b4d1a1a0a12dedba3c3ee6
Kordell Parrish <parrishkordell@gmail.com>
2020-07-31 01:37:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,30 +11,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array element.unshift
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["stuff", "things", "peeps", "eye"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["stuff", "things", "peeps", "eye"]
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["stuff", "things", "peeps", "eye"]
   array[#{index_number}]
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["stuff", "things", "peeps", "eye"]
   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["stuff", "things", "peeps", "eye"]
   array[3]
 end
